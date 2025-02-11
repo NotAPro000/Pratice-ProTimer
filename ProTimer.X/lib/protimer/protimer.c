01116462f18a3f1b0ccdd71ef872c314f40dccff
NotAPro000 <james903199066@gmail.com>
2025-02-12 00:50:48 +0800

    Add Protimer Strcture

--- ProTimer.X/lib/protimer/protimer.c
+++ ProTimer.X/lib/protimer/protimer.c
@@ -1,1 +1,34 @@
 #include "./protimer.h"
+
+static ProTimer_t proTimer_Module;
+unsigned long protimer_millisCount = 0;
+
+void ProTimer_Initialize(){
+    proTimer_Module.currState = ProTimer_Idle;
+}
+
+void ProTimer_Millis(void){
+    protimer_millisCount++;
+}
+
+void ProTimer_Task(void){
+    
+}
+
+static void ProTimer_Handle_Idle(void){
+    
+}
+
+static void ProTimer_Handle_TimeSet(void){
+    
+}
+static void ProTimer_Handle_Stat(void){
+    
+}
+static void ProTimer_Handle_CountDown(void){
+    
+}
+
+static void ProTimer_Handle_Pause(void){
+    
+}
--- ProTimer.X/lib/protimer/protimer.h
+++ ProTimer.X/lib/protimer/protimer.h
@@ -14,7 +14,30 @@ extern "C" {
 
 #include "../../main.h"
 
-
+    extern unsigned long protimer_millisCount;
+    
+    typedef enum:unsigned char{
+        ProTimer_Idle = 0,
+        ProTimer_TimeSet = 1,
+        ProTimer_Stat = 2,
+        ProTimer_CountDown = 3,
+        ProTimer_Pause = 4
+    }ProTimer_State;
+    
+    typedef struct{
+        ProTimer_State currState;
+    }ProTimer_t;
+    
+    void ProTimer_Task(void);
+    void ProTimer_Millis(void);
+    
+    
+    static void ProTimer_Handle_Idle(void);
+    static void ProTimer_Handle_TimeSet(void);
+    static void ProTimer_Handle_Stat(void);
+    static void ProTimer_Handle_CountDown(void);
+    static void ProTimer_Handle_Pause(void);
+    
 
 #ifdef	__cplusplus
 }
