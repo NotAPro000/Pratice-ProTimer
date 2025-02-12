c428cf0245ab0862c9c0e9a4f2e5b2633c0bdebd
NotAPro000 <james903199066@gmail.com>
2025-02-13 00:18:34 +0800

    Add nbproject for project seeting

--- ProTimer.X/dist/default/debug/__eeprom.d
+++
@@ -1,2 +0,0 @@
-dist/default/debug\__eeprom.p1: \
-  D:/Apps/Tools/Microchip/xc8/v3.00/pic/sources/c99/pic/__eeprom.c
--- ProTimer.X/dist/default/debug/compiler_support.d
+++
@@ -1,2 +0,0 @@
-dist/default/debug\compiler_support.p1: \
-  D:/Apps/Tools/Microchip/xc8/v3.00/pic/sources/c99/pic/compiler_support.c
